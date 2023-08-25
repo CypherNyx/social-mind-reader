47d28a46de68a7bd0cc45856f8e08a23fc79363d
Dahlia Guido <dguido.dev@gmail.com>
2023-08-25 14:48:45 -0500

    Ian Darland helped me fix the delete reaction route

--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
-const { usernames, emailAddresses, getRandomThoughts } = require('./data');
+const { usernames, emailAddresses, getRandomThoughts, getThoughtReactions } = require('./data');
 
 connection.on('error', (err) => err);
 
@@ -11,7 +11,7 @@ connection.once('open', async () => {
   if (thoughtCheck.length) {
     await connection.dropCollection('thoughts');
   }
-  
+
   let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
   if (userCheck.length) {
     await connection.dropCollection('users');
@@ -23,7 +23,7 @@ connection.once('open', async () => {
   usernames.forEach((username, i) => {
     const email = emailAddresses[i];
     users.push({
-      username, 
+      username,
       email,
       // Add additional user fields here
     });
@@ -31,6 +31,10 @@ connection.once('open', async () => {
 
   await User.collection.insertMany(users);
   await Thought.collection.insertMany(thoughts);
+
+  // adding a reaction ID for seeded thoughts - Ian Helped me with this part!!!
+  const createdThoughts = await Thought.find({})
+  await Promise.all(createdThoughts.map(item => Thought.findOneAndUpdate({ _id: item._id }, { $push: { reactions: [...getThoughtReactions(3)] } })));
 
   // loop through the saved thoughts, for each thought we need to generate a thought response and insert the thought responses
   console.table(users);
