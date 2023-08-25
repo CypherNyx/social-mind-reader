75326fb05080d1943e5120424f8e470f5f83dbbc
Dahlia Guido <dguido.dev@gmail.com>
2023-08-24 21:28:29 -0500

    complete User model

--- models/User.js
+++ models/User.js
@@ -3,14 +3,31 @@ const { Schema, model } = require('mongoose');
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: String,
-    last: String,
-    age: Number,
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      // validate email
+      match: [/.+@.+\..+/, "Must enter a valid email address!"]
+    },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
         ref: 'Thought',
       },
+
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+      }
     ],
   },
   {
@@ -23,19 +40,11 @@ const userSchema = new Schema(
   }
 );
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('fullName')
-  // Getter
-  .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
+// Create a virtual property `FriendCount` that gets and sets the user's total amount of "friends"
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
 
 // Initialize our User model
 const User = model('user', userSchema);
