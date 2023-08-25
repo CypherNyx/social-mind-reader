2dab5b01a40f19c9c899dcdc461d200025cc8e01
Dahlia Guido <dguido.dev@gmail.com>
2023-08-24 22:43:28 -0500

    adding the update and delete user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,27 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  // Update user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user found with this id!' });
+      }
+
+      res.json(user);
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
   // Delete a user and associated apps
   async deleteUser(req, res) {
     try {
