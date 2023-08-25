82c32d31086f6a812b5a3f737a2cb2ee8b429ca5
Dahlia Guido <dguido.dev@gmail.com>
2023-08-24 22:05:09 -0500

    user and Thought models complete

--- utils/data.js
+++ utils/data.js
@@ -66,6 +66,74 @@ const usernames = [
   'StarStruck',
 ];
 
+const emailAddresses = [
+  'tech.ninja@example.com',
+  'code.wizard@example.com',
+  'pixel.jockey@example.com',
+  'star.gazer@example.com',
+  'data.geek@example.com',
+  'music.maestro@example.com',
+  'adventure.seeker@example.com',
+  'bookworm@example.com',
+  'coffee.addict@example.com',
+  'nature.lover@example.com',
+  'dream.catcher@example.com',
+  'gamer.champion@example.com',
+  'sky.diver@example.com',
+  'foodie.explorer@example.com',
+  'design.enthusiast@example.com',
+  'wanderlust@example.com',
+  'fitness.junkie@example.com',
+  'pet.whisperer@example.com',
+  'movie.buff@example.com',
+  'sunny.soul@example.com',
+  'tech.savvy@example.com',
+  'artistic.mind@example.com',
+  'captain.curious@example.com',
+  'eco.warrior@example.com',
+  'spiritual.being@example.com',
+  'travel.blogger@example.com',
+  'photo.ninja@example.com',
+  'scifi.enthusiast@example.com',
+  'fashionista@example.com',
+  'mindful.thinker@example.com',
+  'happy.camper@example.com',
+  'meme.master@example.com',
+  'rock.star@example.com',
+  'code.crusader@example.com',
+  'puzzle.solver@example.com',
+  'coffee.sipper@example.com',
+  'globe.trotter@example.com',
+  'chef.in.training@example.com',
+  'night.owl@example.com',
+  'nature.adventurer@example.com',
+  'movie.maker@example.com',
+  'fitness.guru@example.com',
+  'animal.lover@example.com',
+  'book.lover@example.com',
+  'gaming.guru@example.com',
+  'wine.connoisseur@example.com',
+  'tech.guru@example.com',
+  'dream.weaver@example.com',
+  'explorer.extraordinaire@example.com',
+  'artistic.soul@example.com',
+  'daring.diver@example.com',
+  'foodie.fanatic@example.com',
+  'music.maven@example.com',
+  'sustainable.soul@example.com',
+  'mindful.traveler@example.com',
+  'mind.over.matter@example.com',
+  'geek.chic@example.com',
+  'mystic.scribe@example.com',
+  'laugh.out.loud@example.com',
+  'coffee.craze@example.com',
+  'curious.cat@example.com',
+  'adventure.awaits@example.com',
+  'pixel.painter@example.com',
+  'data.diva@example.com',
+  'star.struck@example.com',
+];
+
 
 const thoughtDescriptions = [
   'Sunny day, perfect for a walk!',
@@ -112,18 +180,21 @@ const users = [];
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Gets a random full name
+// Gets a random username
 const getRandomName = () =>
   `${getRandomArrItem(usernames)}`;
+
+  // Gets a random email address
+const getRandomEmail = () =>
+`${getRandomArrItem(emailAddresses)}`;
 
 // Function to generate random thoughts that we can add to the database. Includes thought reactions.
 const getRandomThoughts = (int) => {
   let results = [];
   for (let i = 0; i < int; i++) {
     results.push({
-      published: Math.random() < 0.5,
-      description: getRandomArrItem(thoughtDescriptions),
-      buildSuccess: Math.random() < 0.5,
+      thoughtText: getRandomArrItem(thoughtDescriptions),
+      username: getRandomArrItem(usernames),
       reactions: [...getThoughtReactions(3)],
     });
   }
@@ -146,4 +217,4 @@ const getThoughtReactions = (int) => {
 };
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomThoughts };
+module.exports = { usernames, emailAddresses, getRandomName, getRandomThoughts };
